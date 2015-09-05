b6275225f4a265e39d1ad6baf986ad385227c1eb
noscripter <mhz_xz_tc@126.com>
2015-09-05 22:04:13 +0800

    example

---
+++ example/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    "description"      : "python-skeleton",
+    "author"           : "noscripter",
+    "url"              : "https: //github.com/noscripter/python-skeleton",
+    "download_url"     : "https: //github.com/noscripter/python-skeleton",
+    "author_email"     : "noscripter@github.com",
+    "version"          : "0.1",
+    "install_requiers" : ["nose"],
+    "package"          : ["example"],
+    "scripts"          : [],
+    "name"             : "python-skeleton"
+}
+
+setupt(**config)
